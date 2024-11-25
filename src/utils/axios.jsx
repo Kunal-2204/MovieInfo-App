c4be0618ffeffdfeb22de7c8b37c9df8ebeff1c7
Kunal-2204 <152432834+Kunal-2204@users.noreply.github.com>
2024-11-25 22:59:57 +0530

    Update axios.jsx

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -5,7 +5,7 @@ const instance = axios.create({
   headers: {
     accept: "application/json",
     Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMjU4NjU1MGIxNTcwZDYwODY5YzIxMjQxN2ZlNGQ1NyIsIm5iZiI6MTcyNTkxODM1Mi4xODgxNywic3ViIjoiNjZkZjZiNGU5YjVjN2E4ZjNkODc5ZmM4Iiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.5veZWxwYqBaAwbwkHfaO8laI-xMtV2gtyZf0zSZT8q4",
+      "",
   },
 });
 
